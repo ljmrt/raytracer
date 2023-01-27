0f347053e3585a37671f7f14ed034f13882c134e
Lucas Merritt <lucas.j.merritt@gmail.com>
2023-01-27 15:51:15 -0700

    refactor and switch surface to renderer

--- main.c
+++ main.c
@@ -1,44 +1,55 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
+#include <time.h>
+#include <errno.h>
 
 #define WINDOW_WIDTH 640  // window width
 #define WINDOW_HEIGHT 480  //window height
 
 int main(int argc, char* args[]) {
     SDL_Window *window = NULL;
-    SDL_Surface *window_surface = NULL;
+    SDL_Renderer *pixel_renderer = NULL;
 
     int init_result = SDL_Init(SDL_INIT_VIDEO);
-    if (init_result < 0) goto SDL_ERROR;
+    if (init_result < 0) {
+        printf("sdl initialization error: %s\n", SDL_GetError());
+        goto MAIN_END;
+    }
 
     window = SDL_CreateWindow("raytracer",
         SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
         WINDOW_WIDTH, WINDOW_HEIGHT, 
         SDL_WINDOW_SHOWN);
-    if (window == NULL) goto SDL_ERROR;
+    if (window == NULL) {
+        printf("window creation error: %s\n", SDL_GetError());
+        goto MAIN_END;
+    }
 
-    window_surface = SDL_GetWindowSurface(window);
+    pixel_renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_SOFTWARE);
+    if (pixel_renderer == NULL) {
+        printf("renderer creation error: %s\n", SDL_GetError());
+        goto MAIN_END;
+    }
 
-    SDL_FillRect(window_surface, NULL, SDL_MapRGB(window_surface->format, 0xFF, 0xFF, 0xFF));  // fill the window surface white
-    SDL_UpdateWindowSurface(window);
+    SDL_SetRenderDrawColor(pixel_renderer, 0xFF, 0xFF, 0xFF, 0xFF);
+    SDL_RenderClear(pixel_renderer);
+    SDL_RenderPresent(pixel_renderer);
 
     // keep the window up
     SDL_Event poll_event;
-    int quit = 0;
-    while (!quit) {
+    while (1 < 2) {
         SDL_PollEvent(&poll_event);  // poll for key presses 
         // if quit key pressed
         if (poll_event.key.keysym.sym == SDLK_ESCAPE) {
-            quit = 1;
+            break;
         }
     }
 
-    SDL_DestroyWindow(window);
+    MAIN_END:
+        if (window != NULL) {
+            SDL_DestroyWindow(window);
+        }
+        SDL_Quit();
 
-    SDL_ERROR:
-        printf("sdl error: %s\n", SDL_GetError());
-
-    SDL_Quit();
-
-    return 0;
+        return 0;
 }
