09cc1cf0a126e022125d6c2789d49b9bcbcfa2e7
Lucas Merritt <lucas.j.merritt@gmail.com>
2023-01-26 18:59:32 -0700

    code improvements

--- main.c
+++ main.c
@@ -1,25 +1,25 @@
 #include <SDL2/SDL.h>
 #include <stdio.h>
 
-#define SCREEN_WIDTH 640  // window width
-#define SCREEN_HEIGHT 480  //window height
+#define WINDOW_WIDTH 640  // window width
+#define WINDOW_HEIGHT 480  //window height
 
 int main(int argc, char* args[]) {
     SDL_Window *window = NULL;
-    SDL_Surface *screen_surface = NULL;
+    SDL_Surface *window_surface = NULL;
 
     int init_result = SDL_Init(SDL_INIT_VIDEO);
     if (init_result < 0) goto SDL_ERROR;
 
     window = SDL_CreateWindow("raytracer",
-        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, 
-        SCREEN_WIDTH, SCREEN_HEIGHT, 
+        SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED,
+        WINDOW_WIDTH, WINDOW_HEIGHT, 
         SDL_WINDOW_SHOWN);
     if (window == NULL) goto SDL_ERROR;
 
-    screen_surface = SDL_GetWindowSurface(window);
+    window_surface = SDL_GetWindowSurface(window);
 
-    SDL_FillRect(screen_surface, NULL, SDL_MapRGB(screen_surface->format, 0xFF, 0xFF, 0xFF));  //fill the window surface white
+    SDL_FillRect(window_surface, NULL, SDL_MapRGB(window_surface->format, 0xFF, 0xFF, 0xFF));  // fill the window surface white
     SDL_UpdateWindowSurface(window);
 
     // keep the window up
